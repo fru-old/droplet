4d7835933ddb54dfbc2cb4e0814daf0000c6662f
Florian Rüberg <florian.rueberg@gmail.com>
2017-08-24 21:46:48 +0200

    Extend target builder

--- droplet-tree/iterator/BuildTargetAreas.js
+++ droplet-tree/iterator/BuildTargetAreas.js
@@ -4,29 +4,50 @@ import { getHoverInfo } from '../helper/hover';
 
 export class BuildTargetAreas extends AbstractReverseIterator {
 
-  targets = [];
+  results = [];
+  incomplete = {
+    prev: [],
+    prevNotSelected: [],
+    currentRow: []
+  };
+  history = {
+    next: null,
+    nextNotSelected: null
+  }
+
   start(tree, options) {
     this.iterateReverse(tree, null, options);
-    return this.targets;
+    return this.results;
   }
+  set(array, call) { array.forEach(call); array.length = 0; }
+  add(array, targets) { array.push.apply(array, targets); }
 
-  notSetBefore = [];
+  // New rows is assigned as prev and possibly prevNotSelected
+
   enterRow(row, path, selected) {
+    this.set(this.incomplete.prev, x => x.prev = {path, row});
     if (selected) return;
-    this.notSetBefore.forEach(c => c.beforePath = path);
-    this.notSetBefore = [];
+    this.set(this.incomplete.prevNotSelected, x => x.prevNotSelected = {path, row});
   }
 
-  previous = null;
+  // On every exit we can assign the history and then make this row history
+
   exitRow(row, path, selected) {
-    if (!selected) this.previous = path;
+    this.set(this.incomplete.currentRow, x => {
+      x.next = this.history.next;
+      x.nextNotSelected = this.history.nextNotSelected;
+    });
+
+    this.history.next = {path, row};
+    if (!selected) this.history.nextNotSelected = {path, row};
   }
 
   visitNode(node, path, selected, options) {
     let targets = this.buildTargetAreas(node, path, selected, options);
-    this.notSetBefore = this.notSetBefore.concat(targets);
-    targets.forEach(c => c.afterPath = this.previous);
-    this.targets = this.targets.concat(targets);
+    this.add(this.results, targets);
+    this.add(this.incomplete.prev, targets);
+    this.add(this.incomplete.prevNotSelected, targets);
+    this.add(this.incomplete.currentRow, targets);
   }
 
   buildTargetAreas(node, path, selected, options) {
@@ -35,8 +56,8 @@ export class BuildTargetAreas extends AbstractReverseIterator {
     var sections = buildTargets(box, options, path.isFirstInRow() ? leftSpacing : 0);
 
     function preview(direction, offset) {
-      let before = this.beforePath;
-      let after  = this.afterPath;
+      let before = this.prev && this.prev.path;
+      let after  = this.next && this.next.path;
       let info = getHoverInfo(path, before, after, offset, options);
 
       return {levelDifference: info.level - path.getLevel(), box, direction, selected};
